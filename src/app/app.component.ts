0245a80b5ef32608b95a0a53bbe2f4c129b4f70c
Giacomo <Giacomo@GIACOMO-PC>
2020-02-23 21:07:47 +0100

    Inizializazione

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Ag9Test';
+  
+  numeroRighe: number;
+  numeroColonne: number;
+  padding: number;
+
+  margineAlto: number;
+  margineBasso: number;
+  margineDestra: number;
+  margineSinistra: number;
+
+  numeroElementi:number;
+
+
+  constructor() {
+    this.numeroRighe = 2;
+    this.numeroColonne = 2;
+    this.padding = 0.2;
+
+    this.margineAlto = 2.5;
+    this.margineBasso = 2;
+    this.margineDestra = 1;
+    this.margineSinistra = 1;
+
+    this.numeroElementi = 8;
+
+    
+  }
+
+  elementi(pag:number) {
+    const pageSize= this.numeroColonne*this.numeroRighe;
+    return  Array(this.numeroElementi).slice((pag-1) * pageSize, pag * pageSize);
+  }
+
+  numeroPagine(){
+    return Array(Math.ceil(this.numeroElementi/(this.numeroRighe*this.numeroColonne)));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MatInputModule  } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+
+import { FormsModule } from '@angular/forms';
+
+import { PDFExportModule } from '@progress/kendo-angular-pdf-export';
 
 @NgModule({
   declarations: [
@@ -10,7 +21,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    FlexLayoutModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    PDFExportModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
